c3e4922a01f9134cc641b5ca538de0ec89b8f651
gustavoshitara <gus_port@yahoo.com.br>
2019-05-27 19:54:44 -0300

    Inclusão do botão de login para diferenciar do botão de criar conta e envio do campo email para as próximas telas

--- app/src/main/java/br/com/digitalhouse/teste/LoginActivity.java
+++ app/src/main/java/br/com/digitalhouse/teste/LoginActivity.java
@@ -12,6 +12,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private TextInputLayout textInputLayoutEmail;
     private TextInputLayout textInputLayoutPassword;
+    private Button btnNewAccount;
     private Button btnLogin;
     //private Button btnLoginFacebook;
     //private Button btnLoginGoogle;
@@ -24,10 +25,17 @@ public class LoginActivity extends AppCompatActivity {
 
         initViews();
 
+        btnNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateAndGoToRegister();
+            }
+        });
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validateAndGoToNextActivity();
+                validateAndGoToProfile();
             }
         });
     }
@@ -35,12 +43,13 @@ public class LoginActivity extends AppCompatActivity {
     private void initViews(){
         textInputLayoutEmail = findViewById(R.id.textInputLayoutEmail);
         textInputLayoutPassword = findViewById(R.id.textInputLayoutPassword);
+        btnNewAccount = findViewById(R.id.btnNewAccount);
         btnLogin = findViewById(R.id.btnLogin);
         //btnLoginFacebook = findViewById(R.id.btnLoginFacebook);
         //btnLoginGoogle = findViewById(R.id.btnLoginGoogle);
     }
 
-    private void validateAndGoToNextActivity(){
+    private void validateAndGoToRegister(){
         String email = textInputLayoutEmail.getEditText().getText().toString();
         String password = textInputLayoutPassword.getEditText().getText().toString();
 
@@ -54,13 +63,44 @@ public class LoginActivity extends AppCompatActivity {
             return;
         }
 
-        /*Comando para seguir para a tela de cadastro quando o botão 'Criar Senha' for clicado
         if(email != null && password != null){
-            Intent intent = new Intent(LoginActivity.this,ProfileActivity.class);
+            Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("EMAIL",email);
+            intent.putExtras(bundle);
+
             startActivity(intent);
         }else {
             Snackbar.make(textInputLayoutEmail, "Email ou Senha inválidos", Snackbar.LENGTH_SHORT);
         }
-        */
+
     }
+
+    private void validateAndGoToProfile(){
+        String email = textInputLayoutEmail.getEditText().getText().toString();
+        String password = textInputLayoutPassword.getEditText().getText().toString();
+
+        if(email.isEmpty()){
+            textInputLayoutEmail.setError("Email não pode ser vazio");
+            return;
+        }
+
+        if(password.isEmpty()){
+            textInputLayoutPassword.setError("Senha não pode ser vazia");
+            return;
+        }
+
+        if(email != null && password != null){
+            Intent intent = new Intent(LoginActivity.this,ProfileActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("EMAIL",email);
+            intent.putExtras(bundle);
+
+            startActivity(intent);
+        }else {
+            Snackbar.make(textInputLayoutEmail, "Email ou Senha inválidos", Snackbar.LENGTH_SHORT);
+        }
+
+    }
+
 }
